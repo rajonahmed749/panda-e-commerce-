80c78971745e0dd7ed0b2fce12c45202b4e0a4ba
rajonahmed749 <raazon749@gmail.com>
2021-09-25 14:39:40 +0600

    almost done

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Container, Row, Col ,Table} from 'react-bootstrap';
+import { Container, Row, Col, Table } from 'react-bootstrap';
 
 
 const Admin = () => {
 
-    
+
     const [orders, setOrders] = useState([])
-    
+
     useEffect(() => {
         fetch(`http://localhost:4000/allOrders`)
             .then(res => res.json())
@@ -14,34 +14,36 @@ const Admin = () => {
     }, [])
     console.log("orders form db", orders)
 
-    let count= 1;
+    let count = 1;
     return (
         <Container>
             <Row className="justify-content-center mt-5 ">
-                <Col sm={3}>Navbar</Col>
+                <Col sm={3}>
+                    <p>N.B: This is only for admin panel. </p>
+                </Col>
                 <Col sm={7}>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Order No.</th>
                                 <th>Shoe Model</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
+                                <th>Price</th>
+                                <th>Payment Status</th>
                             </tr>
                         </thead>
                         <tbody>
-                        {
-                            orders.map(info => {
-                                return (
-                                    <tr key={info._id}>
-                                        <td> {count++} </td>
-                                        <td>{info.shoeName}</td>
-                                        <td>${info.price}</td>
-                                        <td><button className="btn btn-danger">Delete</button> </td>
-                                    </tr>
-                                )
-                            })
-                        }                            
+                            {
+                                orders.map(info => {
+                                    return (
+                                        <tr key={info._id}>
+                                            <td>{count++}</td>
+                                            <td>{info.shoeName}</td>
+                                            <td>${info.price}</td>
+                                            <td className="text-center"><button className="btn btn-success">{info.status}</button> </td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </Table>
                 </Col>
